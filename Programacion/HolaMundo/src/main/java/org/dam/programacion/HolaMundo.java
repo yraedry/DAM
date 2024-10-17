9585d4d95b9f7fa3a32a380b606c477db21c57ad
adrian nuñez <adrianns93@gmail.com>
2024-10-17 17:36:29 +0200

    inicio de estudios superiores

---
+++ Programacion/HolaMundo/src/main/java/org/dam/programacion/HolaMundo.java
@@ -0,0 +1,15 @@
+package org.dam.programacion;
+
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        holaMundo();
+    }
+
+    /**
+     * El metodo escribe por pantalla hola mundo
+     */
+    private static void holaMundo(){
+        System.out.println("Hola Mundo");
+    }
+}
