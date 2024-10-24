3af7e9eb08faab41f1e05f7bbf8097b89079f3d7
adrian nuñez <adrianns93@gmail.com>
2024-10-24 11:49:01 +0200

    cambio de nombre de clase

--- Programacion/javaBasics/src/main/java/org/dam/programacion/basics/ImpresionVariables.java
+++ Programacion/javaBasics/src/main/java/org/dam/programacion/basics/ImpresionVariables.java
@@ -1,6 +1,6 @@
 package org.dam.programacion.basics;
 
-public class ejercicio0 {
+public class ImpresionVariables {
     public static void main(String[] args) {
         int i = 10;
         System.out.println("El valor de i es de "+ i);
