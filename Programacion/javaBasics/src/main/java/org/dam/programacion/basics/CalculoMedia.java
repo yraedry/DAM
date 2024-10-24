a1e4a0785dc7e1741ef54f7579f2c18b565bf91a
adrian nuñez <adrianns93@gmail.com>
2024-10-24 11:56:02 +0200

    creados nuevos metodos para extraer la logica del main

--- Programacion/javaBasics/src/main/java/org/dam/programacion/basics/CalculoMedia.java
+++ Programacion/javaBasics/src/main/java/org/dam/programacion/basics/CalculoMedia.java
@@ -6,7 +6,11 @@ public class CalculoMedia {
         double num2 = 6.7;
         double num3 = 9.3;
 
-        double media = (num1 + num2 + num3) /3;
+        double media = calculoMedia(num1, num2, num3);
         System.out.println(media);
+    }
+
+    private static double calculoMedia(double num1, double num2, double num3){
+        return (num1 + num2 + num3) /3;
     }
 }
--- Programacion/javaBasics/src/main/java/org/dam/programacion/basics/Conversiones.java
+++ Programacion/javaBasics/src/main/java/org/dam/programacion/basics/Conversiones.java
@@ -3,10 +3,18 @@ package org.dam.programacion.basics;
 public class Conversiones {
     public static void main(String[] args) {
         int numero = 42;
-        String numeroCadena = String.valueOf(numero);
+        String numeroCadena = transformarCadena(numero);
         String texto = "123";
-        int textoEntero = Integer.parseInt(texto);
+        int textoEntero = transformarEntero(texto);
         System.out.println("El numero transformado a String es " + numeroCadena);
         System.out.println("La cadena transformado a numero es " + textoEntero);
+    }
+
+    private static String transformarCadena(int numero){
+        return String.valueOf(numero);
+    }
+
+    private static int transformarEntero(String texto){
+        return Integer.parseInt(texto);
     }
 }
