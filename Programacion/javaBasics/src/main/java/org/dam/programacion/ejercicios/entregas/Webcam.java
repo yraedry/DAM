9bf0250000c2e1de3dda918ec72ccab7914d39eb
Mayden <adrianns93@gmail.com>
2025-02-12 13:38:57 +0100

    ejercicio webcam

---
+++ Programacion/javaBasics/src/main/java/org/dam/programacion/ejercicios/entregas/Webcam.java
@@ -0,0 +1,69 @@
+package com.example.hello_world.hello_world;
+
+public class Webcam {
+    private String resolucion;
+    private int fps;
+    private boolean esEncendido;
+    private double tamanioArchivo;
+
+    public Webcam(String resolucion, int fps, boolean esEncendido, double tamanioArchivo) {
+        this.resolucion = resolucion;
+        this.fps = fps;
+        this.esEncendido = esEncendido;
+        this.tamanioArchivo = tamanioArchivo;
+    }
+
+    public boolean encender(){
+        System.out.println("Has encendido la webcam");
+        return esEncendido = true;
+    }
+
+    public boolean apagar(){
+        System.out.println("Has apagado la webcam");
+        return esEncendido = false;
+    }
+
+    public void tomarFoto(){
+        if(esEncendido){
+            var alto = Integer.parseInt(resolucion.substring(0, resolucion.indexOf("x")));
+            var ancho = Integer.parseInt(resolucion.substring(resolucion.indexOf("x") + 1));
+            tamanioArchivo = (ancho * alto * fps) / 1000000.0;
+            System.out.println("Tomando foto con la resolucion "+ resolucion + "y el tamaño es "+ tamanioArchivo);
+        }else{
+            System.out.println("Debes encender la webcam antes de tomar una foto");
+        }
+
+    }
+
+    public String getResolucion() {
+        return resolucion;
+    }
+
+    public void setResolucion(String resolucion) {
+        this.resolucion = resolucion;
+    }
+
+    public int getFps() {
+        return fps;
+    }
+
+    public void setFps(int fps) {
+        this.fps = fps;
+    }
+
+    public boolean isEsEncendido() {
+        return esEncendido;
+    }
+
+    public void setEsEncendido(boolean esEncendido) {
+        this.esEncendido = esEncendido;
+    }
+
+    public double isTamanioArchivo() {
+        return tamanioArchivo;
+    }
+
+    public void setTamanioArchivo(double tamanioArchivo) {
+        this.tamanioArchivo = tamanioArchivo;
+    }
+}
